ca92746eefc00492ab57194e06b977935a0da5fe
Louis <lf.gualtero@uniandes.edu.co>
2020-09-15 20:07:21 -0500

    initial commit

---
+++ lib/screens/pagina1_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Pagina1Screen extends StatelessWidget {
+  const Pagina1Screen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('ehelldskfja;'),
+      ),
+    );
+  }
+}
